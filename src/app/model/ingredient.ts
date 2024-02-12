e53e866376e4b8d64168df1eedaa8b33fde54bef
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-12 18:02:58 +0100

    add ingredient model

--- src/app/model/ingredient.ts
+++ src/app/model/ingredient.ts
@@ -1,2 +1,9 @@
 export class Ingredient {
+    name:string ='';
+    amount:number =0
+
+    constructor(name:string, amount:number){
+        this.name = name;
+        this.amount = amount;
+    }
 }
