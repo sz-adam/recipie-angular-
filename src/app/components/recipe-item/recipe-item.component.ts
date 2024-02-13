b1ea7210230d647ae74ecb74c17dde279ba37a73
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-13 17:54:08 +0100

    data transfer between recipe-item and the list (@input())

--- src/app/components/recipe-item/recipe-item.component.ts
+++ src/app/components/recipe-item/recipe-item.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/model/recipe';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.scss']
 })
-export class RecipeItemComponent {
+export class RecipeItemComponent implements OnInit {
+  @Input() recipe!: Recipe;
 
+  constructor(){}
+
+  ngOnInit(){
+
+  }
 }
