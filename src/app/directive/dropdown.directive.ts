02504ae6dfba0a2d8add3b039067a713d509c80a
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-13 19:58:35 +0100

    dropdown create function

--- src/app/directive/dropdown.directive.ts
+++ src/app/directive/dropdown.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-
+@HostBinding('class.open')  isOpen: boolean = false;
+@HostListener('click') toggleOpen(){
+this.isOpen = !this.isOpen
+}
   constructor() { }
 
 }
