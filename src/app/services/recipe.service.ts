ac4e9b5275f28e4a50bda1563dff03d1b53b6d19
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-15 18:14:07 +0100

    display ingredients

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../model/recipe';
+import { Ingredient } from '../model/ingredient';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,8 @@ export class RecipeService {
   constructor() { }
 
   private recipes: Recipe[] = [
-    new Recipe('A test recepie', 'this test ' , 'https://images.pexels.com/photos/6287519/pexels-photo-6287519.jpeg?auto=compress&cs=tinysrgb&w=600'),
-    new Recipe('Ab test recepie', 'this test ' , 'https://images.pexels.com/photos/6287519/pexels-photo-6287519.jpeg?auto=compress&cs=tinysrgb&w=600'),
+    new Recipe('A test recepie', 'this test ' , 'https://images.pexels.com/photos/6287519/pexels-photo-6287519.jpeg?auto=compress&cs=tinysrgb&w=600',[new Ingredient('Meat', 1),new Ingredient('Bread', 2)]),
+    new Recipe('Ab test recepie', 'this test ' , 'https://images.pexels.com/photos/6287519/pexels-photo-6287519.jpeg?auto=compress&cs=tinysrgb&w=600',[new Ingredient('Milch', 2)]),
   ];
 
   getRecipes(){
